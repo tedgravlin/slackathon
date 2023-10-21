c9cbb0e9bf3daa767d14384e138dfa2b3be50c9c
Adrian A. Gonzalez <78124761+Locobird00@users.noreply.github.com>
2023-10-21 14:20:46 -0400

    Updated workLife.html and workLife.js

--- scripts/workLife.js
+++ scripts/workLife.js
@@ -1,1 +1,15 @@
-function doSomething() {}
+const d = new Date();
+const startTime = new Date();
+const endTime = new Date();
+
+function timeProgress() {
+
+    startTime.setHours(12, 0, 0);
+    endTime.setHours(20, 0, 0);
+
+    document.getElementById("time").innerHTML = getProgress();
+}
+
+function getProgress() {
+    return (d.getTime() - startTime.getTime()) / (endTime.getTime() - startTime.getTime());
+}
